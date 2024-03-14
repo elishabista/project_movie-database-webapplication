495190271bd7c8eb77d227e0afe6ec24c0828a47
elishabista <elishabista111@gmail.com>
2024-03-14 11:31:36 +0545

    fix: fixing issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import './App.css'
-import Home from "./components/Home";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import MovieDetail from './components/MovieDetail';
+import Error from './components/Error';
+import { lazy } from 'react';
+const Home = lazy(() => import("./components/Home"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
-  },
+    element: <Home />,
+    errorElement:<Error/>,
+   },
   {
     path:"movie/:id",
     element:<MovieDetail/>
