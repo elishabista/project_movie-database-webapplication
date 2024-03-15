8826cd802efe64041e1f2e7d9233668499ea8865
elishabista <elishabista111@gmail.com>
2024-03-15 15:09:23 +0545

    fix: build the app

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import  { FC } from "react";
 import { Swiper, SwiperProps, SwiperSlide, SwiperSlideProps } from "swiper/react";
 import { A11y, Autoplay, Navigation, Pagination } from "swiper";
 
--- src/core/MovieDetails/MovieDetailsLoading.tsx
+++ src/core/MovieDetails/MovieDetailsLoading.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const MovieDetailsLoading = () => {
   return (
     <div>MovieDetailsLoading</div>
